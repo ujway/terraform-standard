9f53ff8ac5e1fd0947cc55e2ebc623bcc4e8289d
ujway <satoyu313@gmail.com>
2018-03-18 00:38:56 +0900

    modify vpc names

--- aws_vpc.tf
+++ aws_vpc.tf
@@ -11,31 +11,31 @@ resource "aws_vpc" "vpc_main" {
 #####################################
 # Internet Gateway Settings
 #####################################
-resource "aws_internet_gateway" "vpc_main-igw" {
+resource "aws_internet_gateway" "vpc_main_igw" {
   vpc_id = "${aws_vpc.vpc_main.id}"
   tags {
-    Name = "${var.app_identity_name} igw"
+    Name = "${var.app_identity_name}IGW"
   }
 }
 
 #####################################
 # Public Subnets Settings
 #####################################
-resource "aws_subnet" "vpc_main-public-subnet1" {
+resource "aws_subnet" "public_subnet1" {
   vpc_id = "${aws_vpc.vpc_main.id}"
   cidr_block = "${var.public_segment1}"
   availability_zone = "${var.public_segment1_az}"
   tags {
-    Name = "${var.app_identity_name} PUBLIC SUBNET1"
+    Name = "${var.app_identity_name}Subnet1"
   }
 }
 
-resource "aws_subnet" "vpc_main-public-subnet2" {
+resource "aws_subnet" "public_subnet2" {
   vpc_id = "${aws_vpc.vpc_main.id}"
   cidr_block = "${var.public_segment2}"
   availability_zone = "${var.public_segment2_az}"
   tags {
-    Name = "${var.app_identity_name} PUBLIC SUBNET2"
+    Name = "${var.app_identity_name}Subnet2"
   }
 }
 
@@ -46,19 +46,19 @@ resource "aws_route_table" "vpc_main-public-rt" {
   vpc_id = "${aws_vpc.vpc_main.id}"
   route {
     cidr_block = "0.0.0.0/0"
-    gateway_id = "${aws_internet_gateway.vpc_main-igw.id}"
+    gateway_id = "${aws_internet_gateway.vpc_main_igw.id}"
   }
   tags {
-    Name = "${var.app_identity_name} ROUTE TABLE"
+    Name = "${var.app_identity_name}RouteTable"
   }
 }
 
 resource "aws_route_table_association" "vpc_main-rta1" {
-  subnet_id = "${aws_subnet.vpc_main-public-subnet1.id}"
+  subnet_id = "${aws_subnet.public_subnet1.id}"
   route_table_id = "${aws_route_table.vpc_main-public-rt.id}"
 }
 
 resource "aws_route_table_association" "vpc_main-rta2" {
-  subnet_id = "${aws_subnet.vpc_main-public-subnet2.id}"
+  subnet_id = "${aws_subnet.public_subnet2.id}"
   route_table_id = "${aws_route_table.vpc_main-public-rt.id}"
 }
