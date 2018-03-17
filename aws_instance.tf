6f9248ad1bbfd4d076a2b9373092ee9b393723f8
ujway <satoyu313@gmail.com>
2018-03-18 00:43:41 +0900

    add instance resources

---
+++ aws_instance.tf
@@ -0,0 +1,38 @@
+#####################################
+# Instance Settings
+#####################################
+resource "aws_instance" "api" {
+  ami           = "${var.instance_image_id}"
+  instance_type = "${var.api_instance_class}"
+  monitoring    = true
+  key_name      = "${var.key_name}"
+  // TODO associate elastic ip
+
+  tags {
+    Name = "api"
+  }
+}
+
+resource "aws_instance" "batch" {
+  ami           = "${var.instance_image_id}"
+  instance_type = "${var.batch_instance_class}"
+  monitoring    = true
+  key_name      = "${var.key_name}"
+  // TODO associate elastic ip
+
+  tags {
+    Name = "batch"
+  }
+}
+
+resource "aws_instance" "job" {
+  ami           = "${var.instance_image_id}"
+  instance_type = "${var.job_instance_class}"
+  monitoring    = true
+  key_name      = "${var.key_name}"
+  // TODO associate elastic ip
+
+  tags {
+    Name = "job"
+  }
+}
