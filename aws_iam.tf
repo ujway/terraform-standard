5b84bf7834811b8f38db812f88aa033bb8964cc4
ujway <satoyu313@gmail.com>
2018-03-18 00:49:23 +0900

    modify iam ec2 policy

--- aws_iam.tf
+++ aws_iam.tf
@@ -1,20 +1,6 @@
 resource "aws_iam_role" "ec2" {
-  assume_role_policy = <<-JSON
-  {
-    "Version": "2012-10-17",
-    "Statement": [
-      {
-        "Action": "sts:AssumeRole",
-        "Effect": "Allow",
-        "Principal": {
-          "Service": "ec2.amazonaws.com"
-        }
-      }
-    ]
-  }
-  JSON
-
-  name = "${var.app_base_name}_ec2"
+  assume_role_policy = "${file("policies/iam_ec2_policy.json")}"
+  name = "${var.app_base_name}RoleEC2"
 }
 
 resource "aws_iam_instance_profile" "iam_instance_profile" {
