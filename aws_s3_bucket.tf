5402a3b3aa93ef478ace04b9841ca36f2c840f03
ujway <satoyu313@gmail.com>
2018-03-18 00:10:52 +0900

    create host s3 bucket

--- aws_s3_bucket.tf
+++ aws_s3_bucket.tf
@@ -1,8 +1,17 @@
-resource "aws_s3_bucket" "s3_bucket" {
+#####################################
+# S3 Bucket Settings
+#####################################
+resource "aws_s3_bucket" "s3_bucket_host" {
   bucket = "${var.app_base_name}_bucket"
-  acl    = "private"
+  acl    = "public-read"
+  policy = "${file("policies/bucket_policy.json")}"
+
+  website {
+    index_document = "index.html"
+  }
+
   tags {
     Name        = "${var.app_base_name}"
-    Environment = "Dev"
+    Environment = "Dev_V0.1.0"
   }
 }
